9b35825f3577a50bd60fb7edf7a47581475639c3
StarWishsama <starwishsama@outlook.com>
2021-01-30 11:44:55 +0800

    feat: 修改超频方式

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/gadgets/OverclockModule.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/gadgets/OverclockModule.java
@@ -9,13 +9,14 @@ import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.AContainer;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import org.apache.commons.lang.Validate;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Objects;
 import java.util.Optional;
 
 public class OverclockModule extends SimpleSlimefunItem<ItemUseHandler> {
-    private final String OVERCLOCK_PATH = "overclock-multiply";
 
     public OverclockModule(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(category, item, recipeType, recipe);
@@ -25,35 +26,28 @@ public class OverclockModule extends SimpleSlimefunItem<ItemUseHandler> {
     public ItemUseHandler getItemHandler() {
         return e -> {
             Optional<Block> block = e.getClickedBlock();
+            Optional<SlimefunItem> sfBlock = e.getSlimefunBlock();
             e.cancel();
 
-            if (block.isPresent()) {
-                Block b = block.get();
+            if (sfBlock.isPresent() && block.isPresent()) {
+                SlimefunItem item = sfBlock.get();
 
-                //String json = BlockStorage.getBlockInfoAsJson(b);
-                //e.getPlayer().sendMessage(json);
+                if (item instanceof AContainer) {
+                    AContainer machine = (AContainer) item;
+                    AContainer defaultItem = (AContainer) SlimefunItem.getByID(item.getId());
 
-                if (BlockStorage.hasBlockInfo(b)) {
-                    Config cfg = BlockStorage.getLocationInfo(b.getLocation());
+                    Validate.notNull(defaultItem, "超频模块获取默认机器参数失败");
 
-                    SlimefunItem item = SlimefunItem.getByID(cfg.getString("id"));
+                    int multiply = machine.getSpeed() - defaultItem.getSpeed();
 
-                    if (item instanceof AContainer) {
-                        AContainer machine = (AContainer) item;
-                        int multiply = Integer.parseInt(cfg.getString(OVERCLOCK_PATH));
-
-                        if (multiply >= 10) {
-                            e.getPlayer().sendMessage("超频倍率已达上限: 10x");
-                        } else {
-                            cfg.setValue(OVERCLOCK_PATH, multiply + 1);
-                            machine.setProcessingSpeed(machine.getSpeed() * multiply + 1);
-                            e.getPlayer().sendMessage("超频机器成功, 目前倍率: " + machine.getSpeed() + "x");
-                        }
+                    if (multiply >= 10) {
+                        e.getPlayer().sendMessage("超频倍率已达上限: 10x");
                     } else {
-                        e.getPlayer().sendMessage("抱歉, 该方块无法被超频");
+                        machine.setProcessingSpeed(machine.getSpeed() + 1);
+                        e.getPlayer().sendMessage("超频机器成功, 目前倍率: " + machine.getSpeed() + "x");
                     }
                 } else {
-                    e.getPlayer().sendMessage("抱歉, 该方块无法被超频");
+                    e.getPlayer().sendMessage("该机器无法超频");
                 }
             }
         };
