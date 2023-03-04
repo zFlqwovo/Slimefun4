b1d09a6e5478b2a2cd18942fdd616da8f79a048d
StarWishsama <starwishsama@outlook.com>
2023-03-04 20:00:50 +0800

    chor(test): remove player profile related test units

--- src/test/java/io/github/thebusybiscuit/slimefun4/api/profiles/TestPlayerProfile.java
+++ src/test/java/io/github/thebusybiscuit/slimefun4/api/profiles/TestPlayerProfile.java
@@ -1,8 +1,13 @@
 package io.github.thebusybiscuit.slimefun4.api.profiles;
 
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.entity.OfflinePlayerMock;
+import io.github.thebusybiscuit.slimefun4.api.player.PlayerProfile;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import io.github.thebusybiscuit.slimefun4.test.TestUtilities;
 import java.util.Iterator;
 import java.util.Optional;
-
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.junit.jupiter.api.AfterAll;
@@ -10,15 +15,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import io.github.thebusybiscuit.slimefun4.api.items.HashedArmorpiece;
-import io.github.thebusybiscuit.slimefun4.api.player.PlayerProfile;
-import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
-import io.github.thebusybiscuit.slimefun4.test.TestUtilities;
-
-import be.seeseemelk.mockbukkit.MockBukkit;
-import be.seeseemelk.mockbukkit.ServerMock;
-import be.seeseemelk.mockbukkit.entity.OfflinePlayerMock;
 
 class TestPlayerProfile {
 
@@ -85,31 +81,6 @@ class TestPlayerProfile {
         Iterator<PlayerProfile> iterator = PlayerProfile.iterator();
         Assertions.assertTrue(iterator.hasNext());
         Assertions.assertEquals(profile, iterator.next());
-    }
-
-    @Test
-    @DisplayName("Test PlayerProfile methods")
-    void testAttributes() throws InterruptedException {
-        Player player = server.addPlayer();
-        PlayerProfile profile = TestUtilities.awaitProfile(player);
-
-        Assertions.assertNotNull(profile.getConfig());
-
-        Assertions.assertFalse(profile.isDirty());
-        profile.markDirty();
-        Assertions.assertTrue(profile.isDirty());
-
-        Assertions.assertFalse(profile.isMarkedForDeletion());
-        profile.markForDeletion();
-        Assertions.assertTrue(profile.isMarkedForDeletion());
-
-        HashedArmorpiece[] armor = profile.getArmor();
-        Assertions.assertEquals(4, armor.length);
-
-        Assertions.assertNotNull(armor[0]);
-        Assertions.assertNotNull(armor[1]);
-        Assertions.assertNotNull(armor[2]);
-        Assertions.assertNotNull(armor[3]);
     }
 
     @Test
