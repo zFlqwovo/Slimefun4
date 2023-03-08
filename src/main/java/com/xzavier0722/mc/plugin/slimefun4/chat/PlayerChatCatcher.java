c7a86b13ca99acc29f0a90892e6bb91a880e0d8d
Xzavier0722 <Xzavier0722@gmail.com>
2023-03-08 19:46:28 +0800

    Revert "pref(backpack): use builtin `awaitInput`"
    This reverts commit cd48cc0233a1fd9d7ba80779298658553b8282fc.

---
+++ src/main/java/com/xzavier0722/mc/plugin/slimefun4/chat/PlayerChatCatcher.java
@@ -0,0 +1,28 @@
+package com.xzavier0722.mc.plugin.slimefun4.chat;
+
+import com.xzavier0722.mc.plugin.slimefun4.chat.listener.PlayerChatListener;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import org.bukkit.Bukkit;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+public class PlayerChatCatcher {
+    private final Map<UUID, Consumer<String>> catchers;
+
+    public PlayerChatCatcher(Slimefun plugin) {
+        catchers = new HashMap<>();
+        Bukkit.getPluginManager().registerEvents(new PlayerChatListener(), plugin);
+    }
+
+    public void scheduleCatcher(UUID pUuid, Consumer<String> handler) {
+        catchers.put(pUuid, handler);
+    }
+
+    public Optional<Consumer<String>> pollCatcher(UUID pUuid) {
+        return Optional.ofNullable(catchers.remove(pUuid));
+    }
+}
---
+++ src/main/java/com/xzavier0722/mc/plugin/slimefun4/chat/listener/PlayerChatListener.java
@@ -0,0 +1,25 @@
+package com.xzavier0722.mc.plugin.slimefun4.chat.listener;
+
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerChatListener implements Listener {
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onChat(AsyncPlayerChatEvent e) {
+        Slimefun.getChatCatcher().pollCatcher(e.getPlayer().getUniqueId()).ifPresent(h -> {
+            e.setCancelled(true);
+            Slimefun.runSync(() -> h.accept(e.getMessage()));
+        });
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onLeave(PlayerQuitEvent e) {
+        Slimefun.getChatCatcher().pollCatcher(e.getPlayer().getUniqueId());
+    }
+
+}
