5b219f5a4a9e69a6b1e5454537bfe8c4fd2e74d9
ybw0014 <i@guizhanss.com>
2023-04-03 05:11:51 -0400

    feat(command): Add blockdata command

---
+++ src/main/java/net/guizhanss/slimefun4/utils/ChatUtils.java
@@ -0,0 +1,37 @@
+package net.guizhanss.slimefun4.utils;
+
+import io.github.bakedlibs.dough.common.ChatColors;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import org.bukkit.command.CommandSender;
+
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+/**
+ * 聊天相关方法
+ * @author ybw0014
+ */
+public class ChatUtils {
+    private ChatUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    /**
+     * 发送带 Slimefun 前缀的消息
+     * @param sender 消息接收人
+     * @param message 消息
+     */
+    public static void sendMessage(CommandSender sender, String message) {
+        sender.sendMessage(Slimefun.getLocalization().getChatPrefix() + ChatColors.color(message));
+    }
+
+    /**
+     * 发送带 Slimefun 前缀的消息
+     * @param sender 消息接收人
+     * @param message 消息
+     * @param function 对消息进行处理的{@link Function}
+     */
+    public static void sendMessage(CommandSender sender, String message, UnaryOperator<String> function) {
+        sendMessage(sender, function.apply(message));
+    }
+}
