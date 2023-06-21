6ed99854e0400caca8b8e76d549e21b421683538
StarWishsama <starwishsama@outlook.com>
2023-06-21 20:44:20 +0800

    chore: add warning for unsupported plugins

--- src/main/java/city/norain/slimefun4/SlimefunExtended.java
+++ src/main/java/city/norain/slimefun4/SlimefunExtended.java
@@ -2,10 +2,8 @@ package city.norain.slimefun4;
 
 import city.norain.slimefun4.listener.SlimefunMigrateListener;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Nonnull;
-import org.bukkit.Bukkit;
 
 public final class SlimefunExtended {
     private static Logger logger = null;
@@ -14,11 +12,14 @@ public final class SlimefunExtended {
 
     public static void register(@Nonnull Slimefun sf) {
         logger = sf.getLogger();
-        if (checkHybridServer(sf)) {
+
+        EnvironmentChecker.checkUnsupportedPlugins(sf, logger);
+
+        if (EnvironmentChecker.checkHybridServer(sf, logger)) {
             return;
         }
 
-        scheduleSlimeGlueCheck(sf);
+        EnvironmentChecker.scheduleSlimeGlueCheck(sf, logger);
         VaultIntegration.register(sf);
 
         migrateListener.register(sf);
@@ -33,26 +34,5 @@ public final class SlimefunExtended {
 
     public static Logger getLogger() {
         return logger;
-    }
-
-    private static boolean checkHybridServer(@Nonnull Slimefun sf) {
-        try {
-            Class.forName("net/minecraftforge/common/MinecraftForge");
-            logger.log(Level.WARNING, "检测到正在使用混合端, Slimefun 将会被禁用!");
-            Bukkit.getPluginManager().disablePlugin(sf);
-
-            return true;
-        } catch (ClassNotFoundException ignored) {
-            return false;
-        }
-    }
-
-    private static void scheduleSlimeGlueCheck(Slimefun sf) {
-        Bukkit.getScheduler().runTaskLater(sf, () -> {
-            if (Bukkit.getPluginManager().getPlugin("SlimeGlue") == null) {
-                logger.log(Level.WARNING, "检测到没有安装 SlimeGlue (粘液胶), 你将缺失对一些插件的额外保护检查!");
-                logger.log(Level.WARNING, "下载: https://github.com/Xzavier0722/SlimeGlue");
-            }
-        }, 300); // 15s
     }
 }
