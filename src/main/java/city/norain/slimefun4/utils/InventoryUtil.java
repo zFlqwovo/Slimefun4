a89a9a3eaa5e028241b9a32f0720a946190a903c
Xzavier0722 <Xzavier0722@gmail.com>
2023-07-11 23:54:07 +0800

    fix(util): exception while closing inv

--- src/main/java/city/norain/slimefun4/utils/InventoryUtil.java
+++ src/main/java/city/norain/slimefun4/utils/InventoryUtil.java
@@ -1,8 +1,9 @@
 package city.norain.slimefun4.utils;
 
-import java.util.Collections;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.inventory.Inventory;
+
+import java.util.LinkedList;
 
 public class InventoryUtil {
     /**
@@ -12,7 +13,7 @@ public class InventoryUtil {
      */
     public static void closeInventory(Inventory inventory) {
         if (inventory != null) {
-            Collections.unmodifiableList(inventory.getViewers()).forEach(HumanEntity::closeInventory);
+            new LinkedList<>(inventory.getViewers()).forEach(HumanEntity::closeInventory);
         }
     }
 }
