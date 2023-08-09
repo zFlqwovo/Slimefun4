cfa26e83a75bb4d07a8f16a2774cf1d25e2213fc
StarWishsama <starwishsama@outlook.com>
2023-08-09 11:19:08 +0800

    Revert sqlite change

--- src/main/java/com/xzavier0722/mc/plugin/slimefun4/storage/adapter/sqlite/SqliteAdapter.java
+++ src/main/java/com/xzavier0722/mc/plugin/slimefun4/storage/adapter/sqlite/SqliteAdapter.java
@@ -6,8 +6,8 @@ import com.xzavier0722.mc.plugin.slimefun4.storage.common.DataScope;
 import com.xzavier0722.mc.plugin.slimefun4.storage.common.DataType;
 import com.xzavier0722.mc.plugin.slimefun4.storage.common.RecordKey;
 import com.xzavier0722.mc.plugin.slimefun4.storage.common.RecordSet;
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 import static com.xzavier0722.mc.plugin.slimefun4.storage.adapter.sqlcommon.SqlConstants.FIELD_BACKPACK_ID;
@@ -26,12 +26,12 @@ import static com.xzavier0722.mc.plugin.slimefun4.storage.adapter.sqlcommon.SqlC
 import static com.xzavier0722.mc.plugin.slimefun4.storage.adapter.sqlcommon.SqlConstants.FIELD_SLIMEFUN_ID;
 
 public class SqliteAdapter implements IDataSourceAdapter<SqliteConfig> {
-    private SqliteConfig config;
+    private HikariDataSource ds;
     private Connection conn;
 
     @Override
     public void prepare(SqliteConfig config) {
-        this.config = config;
+        ds = config.createDataSource();
         conn = createConn();
     }
 
@@ -279,10 +279,8 @@ public class SqliteAdapter implements IDataSourceAdapter<SqliteConfig> {
 
     private Connection createConn() {
         try {
-            // Manually re-trigger sqlite driver init avoid driver list is empty.
-            Class.forName("org.sqlite.JDBC");
-            return DriverManager.getConnection("jdbc:sqlite:" + config.path() + "?foreign_keys=on");
-        } catch (SQLException | ClassNotFoundException e) {
+            return ds.getConnection();
+        } catch (SQLException e) {
             throw new IllegalStateException("Failed to create Sqlite connection: ", e);
         }
     }
