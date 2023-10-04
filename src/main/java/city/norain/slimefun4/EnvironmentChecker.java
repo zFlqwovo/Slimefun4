1961743b1255675a4ad04daf81a751f1eca88648
StarWishsama <starwishsama@outlook.com>
2023-10-04 19:03:50 +0800

    chore: add fabric server check

--- src/main/java/city/norain/slimefun4/EnvironmentChecker.java
+++ src/main/java/city/norain/slimefun4/EnvironmentChecker.java
@@ -4,6 +4,7 @@ import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import org.bukkit.Bukkit;
 
@@ -11,32 +12,35 @@ class EnvironmentChecker {
     private static final List<String> UNSUPPORTED_PLUGINS = List.of("BedrockTechnology", "SlimefunFix", "SlimefunBugFixer", "Slimefunbookfix", "MiraiMC");
 
     static boolean checkIncompatiblePlugins(@Nonnull Slimefun sf, @Nonnull Logger logger) {
-        for (String name : UNSUPPORTED_PLUGINS) {
-            if (sf.getServer().getPluginManager().getPlugin(name) != null) {
-                printBorder(logger);
-                logger.log(Level.WARNING, "");
-                logger.log(Level.WARNING, "");
-                logger.log(Level.WARNING, "检测到安装了 {0}, 该插件已不再兼容新版 Slimefun, 可能会带来不良效果!", name);
-                logger.log(Level.WARNING, "");
-                logger.log(Level.WARNING, "");
-                printBorder(logger);
-                Bukkit.getPluginManager().disablePlugin(sf);
-                return true;
-            }
+        List<String> plugins = UNSUPPORTED_PLUGINS.stream().filter(name -> sf.getServer().getPluginManager().getPlugin(name) != null).toList();
+
+        if (plugins.isEmpty()) {
+            return false;
         }
 
-        return false;
+        printBorder(logger);
+        logger.log(Level.WARNING, "");
+        logger.log(Level.WARNING, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        logger.log(Level.WARNING, "检测到不兼容的插件, 已自动禁用 Slimefun!");
+        logger.log(Level.WARNING, "插件列表: ", String.join(", ", plugins));
+        logger.log(Level.WARNING, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        logger.log(Level.WARNING, "");
+        printBorder(logger);
+        Bukkit.getPluginManager().disablePlugin(sf);
+
+        return true;
     }
 
     static boolean checkHybridServer(@Nonnull Slimefun sf, @Nonnull Logger logger) {
         try {
             Class.forName("net.minecraftforge.common.MinecraftForge");
+            Class.forName("net.fabricmc.loader.impl.launch.server.FabricServerLauncher");
 
             printBorder(logger);
             logger.log(Level.WARNING, "");
-            logger.log(Level.WARNING, "");
+            logger.log(Level.WARNING, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             logger.log(Level.WARNING, "检测到正在使用混合端, Slimefun 将会被禁用!");
-            logger.log(Level.WARNING, "");
+            logger.log(Level.WARNING, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             logger.log(Level.WARNING, "");
             printBorder(logger);
 
