c01a96965205ec4c892a5dc3e7aa2a63a2152fd0
StarWishsama <starwishsama@outlook.com>
2023-10-26 22:04:54 +0800

    fix(android): ensure android status before execute instruction

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/ProgrammableAndroid.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/ProgrammableAndroid.java
@@ -785,7 +785,6 @@ public class ProgrammableAndroid extends SlimefunItem implements InventoryBlock,
                     index = 0;
                 }
 
-                data.setData("fuel", String.valueOf(fuel - 1));
                 Instruction instruction = Instruction.getInstruction(script[index]);
 
                 if (instruction == null) {
@@ -798,7 +797,11 @@ public class ProgrammableAndroid extends SlimefunItem implements InventoryBlock,
                     return;
                 }
 
-                executeInstruction(instruction, b, menu, data, index);
+                if ("false".equals(data.getData("paused"))) {
+                    executeInstruction(instruction, b, menu, data, index);
+
+                    data.setData("fuel", String.valueOf(fuel - 1));
+                }
             }
         }
     }
@@ -806,6 +809,10 @@ public class ProgrammableAndroid extends SlimefunItem implements InventoryBlock,
     @ParametersAreNonnullByDefault
     private void executeInstruction(
             Instruction instruction, Block b, BlockMenu inv, SlimefunBlockData data, int index) {
+        if ("true".equals(data.getData("paused"))) {
+            return;
+        }
+
         if (getAndroidType().isType(instruction.getRequiredType())) {
             String rotationData = data.getData("rotation");
             BlockFace face = rotationData == null ? BlockFace.NORTH : BlockFace.valueOf(rotationData);
