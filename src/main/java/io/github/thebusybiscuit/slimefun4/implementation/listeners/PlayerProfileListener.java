afa63494c447f7b8b0e6d7fad096a2b249d3e5fe
Xzavier0722 <xzavier0722@gmail.com>
2024-01-17 20:27:10 +0800

    style: spotless

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/PlayerProfileListener.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/PlayerProfileListener.java
@@ -2,15 +2,14 @@ package io.github.thebusybiscuit.slimefun4.implementation.listeners;
 
 import io.github.thebusybiscuit.slimefun4.api.player.PlayerProfile;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import java.util.Optional;
+import javax.annotation.Nonnull;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
-
-import javax.annotation.Nonnull;
-import java.util.Optional;
 
 /**
  * This {@link Listener} removes a {@link PlayerProfile} from memory if the corresponding {@link Player}
