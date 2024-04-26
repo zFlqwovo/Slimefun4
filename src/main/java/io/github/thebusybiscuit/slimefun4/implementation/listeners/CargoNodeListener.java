661cd4f093431d0c551e8aefc5e792ec01d82b2b
Ddggdd135 <1306334428@qq.com>
2024-04-27 07:27:26 +0800

    style: spotless

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/CargoNodeListener.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/CargoNodeListener.java
@@ -14,7 +14,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.Vector;
 
 /**
  * This {@link Listener} is solely responsible for preventing Cargo Nodes from being placed
